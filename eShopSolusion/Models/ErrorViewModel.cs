cd6b89e327adcc7c6fddf3ca8bf4b83b79de0c5b
ASUS <ASUS@DESKTOP-D0AD66R>
2020-02-27 14:53:25 +0700

    create solution

---
+++ eShopSolusion/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eShopSolusion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
